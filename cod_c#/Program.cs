a94f0aa17071343cef4f5de7c0c709582217c7b1
pasxin <110281240+pasxin@users.noreply.github.com>
2022-09-16 14:48:21 +0300

    add program_c_code

---
+++ cod_c#/Program.cs
@@ -0,0 +1,21 @@
+﻿// метод вобора элементов маасива
+void SizeArray3 (string [] ArrayFerst)
+{
+    string [] ArrayResult = new string [ArrayFerst.GetLength (0)];
+    Console.Write ("[");
+    int count = 0;
+    for (int i = 0; i < ArrayFerst.GetLength (0); i ++)
+        {
+            if (ArrayFerst[i].Length <= 3)
+                {
+                     ArrayResult [count] = ArrayFerst [i];
+                     Console.Write ($"{ArrayResult [count]} ");
+                     count++;
+                }
+        }
+    Console.WriteLine ("]");
+}
+string[] Array = new string[] {"Привет", "Как", "Дела", "?", "Все" , "Хорошо", "?", "А"};
+SizeArray3 (Array);
+
+ 
